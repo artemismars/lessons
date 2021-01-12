cb90a75af8f3b348a7950b348f8539c3e5383728
artemismars <artemismars@outlook.com>
2021-01-13 08:18:43 +0900

    adding start command

---
+++ practice/index.js
@@ -0,0 +1,10 @@
+// 10 is assigned to variable, x.
+// = is assignment operator.
+// keyword to declare a variable.
+let x = 3;
+let y = 10;
+
+console.log(x);
+x = 15;
+y = 11;
+console.log(x + y);
---
+++ practice/main.js
@@ -0,0 +1,4 @@
+let firstName = `Sandra`;
+console.log(typeof firstName);
+console.log(typeof `Estonia`);
+console.log(typeof 1000);
